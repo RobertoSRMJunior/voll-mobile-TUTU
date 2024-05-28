3457815aff00cdfd74ac6e8942d355a3c26a80e8
Arthurlmr2004 <143120473+Arthurlmr2004@users.noreply.github.com>
2024-05-28 16:59:23 -0300

    Atualização dinâmica dados perfil.
    Atualização dinâmica dados perfil.

--- src/Login.tsx
+++ src/Login.tsx
@@ -18,7 +18,7 @@ export default function Login({ navigation }: any) {
 
   useEffect(() => {
     // Deslogar 
-    //AsyncStorage.removeItem('token')
+    // AsyncStorage.removeItem('token')
     async function verificarLogin() {
       const token = await AsyncStorage.getItem('token')
       if (token) {
@@ -37,7 +37,7 @@ export default function Login({ navigation }: any) {
 
       const tokenDecodificado = jwtDecode(token) as any
       const pacienteId = tokenDecodificado.id
-      AsyncStorage.setItem('pacientId', pacienteId)
+      AsyncStorage.setItem('pacienteId', pacienteId)
       navigation.replace('Tabs')
     }
     else {
